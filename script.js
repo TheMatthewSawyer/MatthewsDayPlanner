616ffc3afaf64bade6e0349d4bb724b9c6222d74
Matthew Sawyer <57121937+matthewleonardsawyer@users.noreply.github.com>
2020-01-09 14:20:31 -0500

    Added functionality for date scroller along with a button that appears on any day in the past/future.

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
+var preWorkArr = new Array(9);
+var workHoursArr = new Array(9);
+var postWorkArr = new Array(6);
+
+var day = {
+    preWork : preWorkArr,
+    workHours : workHoursArr,
+    postWork : postWorkArr
+}
+
+var leftButton = "<button id='dayBackwardBtn'><i class='fas fa-arrow-left'></i></button>";
+var rightButton = "<button id='dayForwardBtn'><i class='fas fa-arrow-right'></i></i></button>";
+var currentDay = moment().format('dddd, MMMM Do');
+var currentView = currentDay;
+var backward = 0;
+var forward = 0;
+
 $(document).ready(function() {
     
     $("#plannerAdd").on("click", function() {
         tabSwitch("#plannerAdd");
-        $("#main").css("backgroundColor", "blue");
+        addToPlanner();
     });
 
     $("#plannerView").on("click", function() {
         tabSwitch("#plannerView");
-        $("#main").css("backgroundColor", "green");
+        viewPlanner();
     });
 
     $("#calendarView").on("click", function() {
         tabSwitch("#calendarView");
-        $("#main").css("backgroundColor", "purple");
+        clear();
     });
 
     $("#plannerAdd").click();
@@ -22,6 +39,83 @@ $(document).ready(function() {
         $("#plannerView").css("backgroundColor", "rgba(250, 250, 210, 0.50)");
         $("#calendarView").css("backgroundColor", "rgba(250, 250, 210, 0.50)");
         $(tab).css("backgroundColor", "rgba(250, 250, 210, 1)");
+    }
+
+    function clear() {
+        $("#dateContainer1").empty();
+        $("#dateContainer2").empty();
+        $("#dateContainer3").empty();
+        $("#main").empty();
+    }
+
+    function addToPlanner() {
+        clear();
+        topDateSelector();
+
+    }
+
+    function dayBackward() {
+        if(forward > 0) {
+            forward--;
+            currentView = moment().add(forward, "days").format('dddd, MMMM Do');
+        } else {
+            backward++;
+            currentView = moment().subtract(backward, "days").format('dddd, MMMM Do');
+        }
+        currDayBtn();
+        return currentView;
+    }
+
+    function dayForward() {
+        if(backward > 0) {
+            backward--;
+            currentView = moment().subtract(backward, "days").format('dddd, MMMM Do');
+        } else {
+            forward++;
+            currentView = moment().add(forward, "days").format('dddd, MMMM Do');
+        }
+        currDayBtn();
+        return currentView;
+    }
+
+    function currDayBtn() {
+        if(currentView === currentDay){
+            $("#curDayBtn").css("visibility", "hidden");
+
+        } else {
+            $("#curDayBtn").css("visibility", "visible");
+        }
+        return;
+    }
+
+    function topDateSelector() {
+        $("#dateContainer1").html(leftButton);
+        $("#dateContainer2").html(currentView);
+        $("#dateContainer3").html(rightButton);
+        
+        $("#dayBackwardBtn").on("click", function() {
+            dayBackward();
+            $("#dateContainer2").html(currentView);
+        });
+        
+        $("#dayForwardBtn").on("click", function() {
+            dayForward();
+            $("#dateContainer2").html(currentView);
+        });
+
+        $("#curDayBtn").on("click", function() {
+            currentView = currentDay;
+            backward = 0;
+            forward = 0;
+            currDayBtn();
+            $("#dateContainer2").html(currentView);
+        });
+    }
+
+    function viewPlanner () {
+        clear();
+        topDateSelector();
+
     }
 
 });
